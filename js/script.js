6e63137dffed9a35117d8edff238242b67950097
mrsunshines <k.hubertjankowski@gmail.com>
2017-11-26 23:53:29 +0100

    lup

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var tweetLink = "https://twitter.com/intent/tweet?text=";
-var quoteUrl = "http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 function getQuote() {
     $.getJSON(quoteUrl, createTweet);
 }
